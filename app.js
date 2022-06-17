80a301c22e0d937af38119d35463a36fcc6ecfc7
capibison <tcapitelli@online.gibz.ch>
2022-06-17 08:48:27 +0200

    full app.js + first handlers

--- app.js
+++ app.js
@@ -1,1 +1,94 @@
-//this is a test hehe
+// 1 - set up express & mongoose
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose')
+
+var fs = require('fs');
+var path = require('path');
+require('dotenv/config');
+
+app.use(express.static(__dirname));
+
+/*
+function bar(() => {
+	console.log('hello')
+})
+
+bar(aFunc)
+*/
+
+// 2 - Verbindung zur Datenbank
+mongoose.connect(process.env.MONGO_URL,
+	{ useNewUrlParser: true, useUnifiedTopology: true }, err => {
+		console.log('connected to ' + process.env.MONGO_URL)
+	});
+
+// 3 - Code in ./models.js
+
+// 4 - Set up EJS
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// EJS als templating engine setzen
+app.set("view engine", "ejs");
+
+// 5 - Set up multer um upload files zu speichern
+var multer = require('multer');
+
+
+var storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'uploads')
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.fieldname + '-' + Date.now())
+	}
+});
+
+var upload = multer({ storage: storage });
+
+// 6 - Mongoose Models
+//var imgModel = require('./model/models');
+const { PRIORITY_ABOVE_NORMAL } = require('constants');
+const { json } = require('body-parser');
+
+// 7 - GET Methods
+app.get('/', (req, res) => {
+		res.render('welcome');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+// 8 - POST Methods
+app.post('/register', upload.single('user'), (req, res, next) => {
+
+	var obj = {
+		username: req.body.username,
+		password: req.body.password,
+		phoneNumber: 0,
+		telephone: 0
+	}
+	userModel.create(obj, (err, item) => {
+		if (err) {
+			console.log(err);
+		}
+		else {
+			res.redirect('/');
+		}
+	});
+});
+
+// Schritt 9 - Den Server port setzen
+var port = process.env.PORT || '3000'
+app.listen(port, err => {
+	if (err)
+		throw err
+	console.log('Server listening on port', port)
+})
